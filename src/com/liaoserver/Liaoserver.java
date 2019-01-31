8425c2d3626b7f2e727a6debe44f902aee11ef02
DrRiver <qiwa3476@colorado.edu>
2019-01-30 19:09:46 -0700

    updated

--- src/com/liaoserver/Liaoserver.java
+++ src/com/liaoserver/Liaoserver.java
@@ -1,40 +1,92 @@
 package com.liaoserver;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Map;
+import java.util.HashMap;
+
 
 
 public class Liaoserver {
 	
-	private String configfile = "liao.conf";
+	public static String CONFIG_FILE = "liao.conf";
 	
+	private BufferedReader bufferedReader;
 	
-	private Map<String, String> conf;
-	private Map<String, Thread> onlineUsers;
+	private HashMap<String, String> confvaluesHashMap;
+	private HashMap<String, Thread> onlineusersHashMap;
+	
 	private ServerSocket serverSocket;
 	
-	class onlineUserThread extends Thread{
-		public onlineUserThread() {
-			// TODO Auto-generated constructor stub
+	class serversocketThread extends Thread{
+		public serversocketThread() {
+			try {
+				serverSocket = new ServerSocket(Integer.parseInt(confvaluesHashMap.get("Port")));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 		
 		@Override
 		public void run() {
-			// TODO Auto-generated method stub
-			try {
-				Socket socket = serverSocket.accept();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			while(!serverSocket.isClosed())
+			{
+				try {
+					Socket socket = serverSocket.accept();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 			super.run();
 		}
 	}
 	
+	class onlineUserThread extends Thread{
+		
+		Socket socket;
+		
+		public onlineUserThread(Socket socket) {
+			this.socket = socket;
+		}
+		
+		@Override
+		public void run() {
+			super.run();
+		}
+	}
+	
 	public Liaoserver() {
+		
+		confvaluesHashMap = new HashMap<String, String>();
+		onlineusersHashMap = new HashMap<String, Thread>();
+		
+	}
+	
+	public int loadConfigFile()
+	{
+		try {
+			bufferedReader = new BufferedReader(new FileReader(CONFIG_FILE));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		String tmpString;
+		String[] splitedStrings;
+		try {
+			while((tmpString = bufferedReader.readLine()) != null)
+			{
+				splitedStrings = tmpString.split("=", 2);
+				confvaluesHashMap.put(splitedStrings[0], splitedStrings[1]);
+			}
+			bufferedReader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return -1;
+		}
+		return 1;
 	}
 	
 	public int startServering()
--- src/com/liaoserver/test.java
+++ src/com/liaoserver/test.java
@@ -11,6 +11,7 @@ import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
 
+
 public class test {
 
 	Map<String, Thread> onlineusersMap;
@@ -37,21 +38,33 @@ public class test {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}*/
-		HashMap<String, String> map = new HashMap<String, String>();
+		/*HashMap<String, String> map = new HashMap<String, String>();
 		map.put("Hello", "Hi");
-		System.out.println(map.get("Hello").toString());
+		System.out.println(map.get("Hello").toString());	
 		try {
 			BufferedReader bufferedReader = new BufferedReader(new FileReader("liao.conf"));
 			String string;
+			String[] strs;
 			while((string = bufferedReader.readLine()) != null)
 			{
 				System.out.println(string);
+				strs = string.split("=", 2);
+				map.put(strs[0], strs[1]);
 			}
+			System.out.println(map.get("Port"));
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+		
+		try {
+			ServerSocket ss = new ServerSocket(9900);
+			ss.close();
+			System.out.println(ss.isClosed());
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
