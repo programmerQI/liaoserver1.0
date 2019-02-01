2fa44038220b05fbce7edb4964b6b1be01baae04
DrRiver <qiwa3476@colorado.edu>
2019-01-31 17:49:51 -0700

    updated

--- src/com/liaoserver/Liaoserver.java
+++ src/com/liaoserver/Liaoserver.java
@@ -4,9 +4,11 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
+import java.util.Vector;
 
 
 
@@ -14,17 +16,23 @@ public class Liaoserver {
 	
 	public static String CONFIG_FILE = "liao.conf";
 	
-	private BufferedReader bufferedReader;
-	
+	private Vector<String> namesStrings;
 	private HashMap<String, String> confvaluesHashMap;
 	private HashMap<String, Thread> onlineusersHashMap;
 	
 	private ServerSocket serverSocket;
 	
-	class serversocketThread extends Thread{
-		public serversocketThread() {
+	
+	class ServersocketThread extends Thread{
+		
+		private BufferedReader bufferedReader;
+		private InputStreamReader inputStreamReader;
+		
+		public ServersocketThread() {
 			try {
+				
 				serverSocket = new ServerSocket(Integer.parseInt(confvaluesHashMap.get("Port")));
+				
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -35,28 +43,55 @@ public class Liaoserver {
 			while(!serverSocket.isClosed())
 			{
 				try {
+					
 					Socket socket = serverSocket.accept();
+					String nameString = getUserInfo(socket);
+					
+					namesStrings.add(nameString);
+					OnlineUserThread newOnlineUserThread = new OnlineUserThread(socket, nameString);
+					onlineusersHashMap.put(nameString, newOnlineUserThread);
+					
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
+				
+
 			}
 			super.run();
 		}
 		
-		public int getUserInfo()
+		public String getUserInfo(Socket socket)
 		{
-			
-			return 1;
+			try {
+				
+				inputStreamReader = new InputStreamReader(socket.getInputStream());
+				bufferedReader = new BufferedReader(inputStreamReader);
+				
+				String string = rot13(bufferedReader.readLine());
+				
+				bufferedReader.close();
+				inputStreamReader.close();
+				
+				return string;
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+				return null;
+			}
 		}
 		
 	}
 	
-	class onlineUserThread extends Thread{
+	class OnlineUserThread extends Thread{
 		
-		Socket socket;
+		private Socket socket;
+		private String nameString;
 		
-		public onlineUserThread(Socket socket) {
+		public OnlineUserThread(Socket socket, String nameString) {
+			
 			this.socket = socket;
+			this.nameString = nameString;
+			
 		}
 		
 		@Override
@@ -67,6 +102,7 @@ public class Liaoserver {
 	
 	public Liaoserver() {
 		
+		namesStrings = new Vector<String>();
 		confvaluesHashMap = new HashMap<String, String>();
 		onlineusersHashMap = new HashMap<String, Thread>();
 		
@@ -74,25 +110,27 @@ public class Liaoserver {
 	
 	public int loadConfigFile()
 	{
+
 		try {
+			BufferedReader bufferedReader;
 			bufferedReader = new BufferedReader(new FileReader(CONFIG_FILE));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		
-		String tmpString;
-		String[] splitedStrings;
-		try {
+			
+			String tmpString;
+			String[] splitedStrings;
 			while((tmpString = bufferedReader.readLine()) != null)
 			{
 				splitedStrings = tmpString.split("=", 2);
 				confvaluesHashMap.put(splitedStrings[0], splitedStrings[1]);
 			}
 			bufferedReader.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return -1;
 		} catch (IOException e) {
 			e.printStackTrace();
 			return -1;
 		}
+		
 		return 1;
 	}
 	
